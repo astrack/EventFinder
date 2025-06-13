71b602476baf5d9af3299050e6cbb58e84f5227b
Andrew Strackbein <starxbein@gmail.com>
2025-06-13 11:36:56 -0400

    added storyblok components and components json

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -6,13 +6,9 @@ import Navbar        from "@/storyblok-components/Navbar";
 import EventCard     from "@/storyblok-components/EventCard";
 import FiltersDrawer from "@/storyblok-components/FiltersDrawer";
 
-console.log(
-  "🪄 Storyblok token seen by Node:",
-  process.env.NEXT_PUBLIC_STORYBLOK_TOKEN?.slice(0, 6) || "<undefined>"
-);
 // storyblokInit RETURNS a getStoryblokApi function ➜ we re-export it
 export const getStoryblokApi = storyblokInit({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN,
+  accessToken: "YSb54AnQcc8czORhorTRVAtt",
   use: [apiPlugin],                 // ← plugin really loaded now
   components: {
     navbar: Navbar,
